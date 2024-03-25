cd97c1a53226b274dcf8f163c7fbcda3ac70252f
Mohammed Gouaouri <mohammed.degouaouri@namla.cloud>
2024-03-25 14:41:37 +0100

    feat: finish solved challenges stats

--- apis/grading/controllers/stats.controller.js
+++ apis/grading/controllers/stats.controller.js
@@ -1,8 +1,14 @@
-import {getCoderHeatmap} from "../services/stats.service.js";
+import {getCoderHeatmap, getSolvedChallengesStats} from "../services/stats.service.js";
 
 
 export const heatmapController = async (req, res) => {
     const {id} = req.user
     const response = await getCoderHeatmap(id)
+    return res.status(response.getHttpStatus()).json(response.getHttpResponse());
+}
+
+export const solvedChallengesController = async (req, res) => {
+    const {id} = req.user
+    const response = await getSolvedChallengesStats(id)
     return res.status(response.getHttpStatus()).json(response.getHttpResponse());
 }
--- apis/grading/services/stats.service.js
+++ apis/grading/services/stats.service.js
@@ -1,12 +1,13 @@
 import {Submission} from "../models/submission.model.js";
 import mongoose from "mongoose";
 import {ServiceResponseSuccess} from "../../common/service_response.js";
+import {Challenge} from "../../challenges/models/Challenge.js";
 
 export const getCoderHeatmap = async (coder_id) => {
     return new ServiceResponseSuccess(
         await Submission.aggregate([
             {
-                $match: {coder: new mongoose.Types.ObjectId(coder_id)}
+                $match: {coder: new mongoose.Types.ObjectId(coder_id), isPassed: true}
             },
             {
                 $addFields: {
@@ -28,6 +29,89 @@ export const getCoderHeatmap = async (coder_id) => {
                     count: 1,
                 }
             }
-        ])
+        ]).exec()
     );
+}
+
+
+export const getSolvedChallengesStats = async (coder_id) => {
+    // return new ServiceResponseSuccess(
+    //
+    // );
+    const totalEasyChallenges = (await Challenge.find({level: 'Easy'}).exec()).length
+    const totalModerateChallenges = (await Challenge.find({level: 'Moderate'}).exec()).length
+    const totalHardChallenges = (await Challenge.find({level: 'Hard'}).exec()).length
+
+    const solved = await Submission.aggregate([
+        {
+            $match: {coder: new mongoose.Types.ObjectId(coder_id), isPassed: true}
+        },
+        {
+            $lookup: {
+                from: 'challenges',
+                localField: 'challenge',
+                foreignField: '_id',
+                as: 'challengeDetails'
+            }
+        },
+        {
+            $unwind: "$challengeDetails" // Unwind to flatten the challenge details array
+        },
+
+        {
+            $group: {
+                _id: null,
+                totalEasySolvedChallenges: {
+                    $sum: {
+                        $cond: [
+                            {$eq: ['$challengeDetails.level', 'Easy']},
+                            1,
+                            0
+                        ]
+                    }
+                },
+                totalModerateSolvedChallenges: {
+                    $sum: {
+                        $cond: [
+                            {$eq: ['$challengeDetails.level', 'Moderate']},
+                            1,
+                            0
+                        ]
+                    }
+                },
+                totalHardSolvedChallenges: {
+                    $sum: {
+                        $cond: [
+                            {$eq: ['$challengeDetails.level', 'Hard']},
+                            1,
+                            0
+                        ]
+                    }
+                },
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                totalEasySolvedChallenges: 1,
+                totalModerateSolvedChallenges: 1,
+                totalHardSolvedChallenges: 1,
+            }
+        }
+    ]).exec()
+    if (solved.length === 0) {
+        return new ServiceResponseSuccess({
+            totalEasyChallenges,
+            totalModerateChallenges,
+            totalHardChallenges
+        })
+    }
+    return new ServiceResponseSuccess(
+        {
+            ...solved[0],
+            totalEasyChallenges,
+            totalModerateChallenges,
+            totalHardChallenges
+        }
+    )
 }
